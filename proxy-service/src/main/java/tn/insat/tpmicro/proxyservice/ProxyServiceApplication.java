5b6cc5532ab65286cb60a79aaa7cdb87fb1b95b5
Amira BA <a.bachour.tn@gmail.com>
2017-12-22 00:04:35 +0100

    tp5

---
+++ proxy-service/src/main/java/tn/insat/tpmicro/proxyservice/ProxyServiceApplication.java
@@ -0,0 +1,14 @@
+package tn.insat.tpmicro.proxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServiceApplication.class, args);
+	}
+}
